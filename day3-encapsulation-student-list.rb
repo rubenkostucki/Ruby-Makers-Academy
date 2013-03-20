98371590e9c5b62b8f7a864a98041449235ee937
Ruben Kostucki <rubenkostucki@gmail.com>
2013-03-20 14:35:54 +0000

    first commit before exercise 2 which involves creating the Students Class

---
+++ day3-encapsulation-student-list.rb
@@ -0,0 +1,73 @@
+#speficifation of this project:
+
+#Create a list so that we can have a reference of students on a particular cohort
+
+#STUDENTLIST
+
+#STUDENTS              [ATTRIBUTE]
+#COHORT                [ATTRIBUTE]
+
+#ADD (STUDENTS)        [INTERACTIONS]
+#REMOVE (STUDENTS)     [INTERACTIONS]
+#VIEW (STUDENTS)       [INTERACTIONS]
+
+#STUDENTS = VARIABLE
+#VIEW (STUDENTS) = METHOD(ARGUMENT)
+
+
+class StudentList #this is the blue print of the class we haven't BUILT yet!
+
+# students --> reason you don't put the attribute INSIDE the class is because you want a different attribute for each StudentList
+# cohort
+
+  def initialize(cohort) #the argument cohort is there because we want the user to input this argument to the method
+
+    #to store cohort we set it as a local variable. It only exists inside that method, and dies at end
+    #student_cohort = cohort
+
+    @student_cohort = cohort #this is an instance variable and has a scope outside of the def initialize method
+    @students = [] #an empty array by default
+  end
+
+  def add(student)
+    # we need to do something here
+    @students.push(student)
+  end
+
+  def remove(student)
+    @students.delete(student)
+  end
+
+  def view_cohort_name #purpose is to return the cohort month
+    @student_cohort #implicit RETURN of the instance variable
+  end
+
+  def view_students
+    @students
+  end
+
+end
+
+march_list = StudentList.new("March Cohort") #this is the process of INSTANTIATION, of building the class for it to now be useful. Process of creating an instance of our student list. This is created in a memory but it's not pointing to anywhere which is why we need to add a variable
+
+puts march_list.view_cohort_name #calling the local method inside the instance variable of the class
+
+march_list.add("Dario")
+march_list.add("Zahid")
+march_list.add("Ben")
+march_list.remove("Dario")
+puts march_list.view_students
+
+
+
+
+
+# puts march_list.view_students
+
+# SETTING COHORT ATTRIBUTE OF STUDENT LIST INSTANCE
+# student_list.cohort = "March"
+
+# GETTING COHORT ATTRIBUTE OF STUDENT LIST INSTANCE
+# puts student_list.cohort
+
+
